314fec99415bc0d57e439a40c5455bcad9951722
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-04-27 19:56:27 +0900

    4

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,17 @@
 <template>
   <h1>A single user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    // console.log(data)
+    // return data.params.id === 1
+    // return data.params.id == 1
+    // return data.params.id == 2
+    // return data.params.id === /^\d+$/.test(data.params.id)
+    return /^\d+$/.test(data.params.id)
+  },
+  layout: 'users'
+}
+</script>
